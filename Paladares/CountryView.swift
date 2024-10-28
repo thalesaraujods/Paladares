d937896622099c9d131a520bd4d8840ae5963004
Thales Araújo <t.araujods@gmail.com>
2024-10-28 11:54:04 -0400

    estruturação do MVMM, criação de pastas e criação do arquivo HomeView.swift

--- Paladares/CountryView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CountryView.swift
-//  Paladares
-//
-//  Created by Amanda Rabelo on 23/10/24.
-//
-
-import Foundation
---
+++ Paladares/Home/HomeView.swift
@@ -0,0 +1,48 @@
+//
+//  HomeView.swift
+//  Paladares
+//
+//  Created by Thales Araújo on 28/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            // Logo e Slogan
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 229, height: 229)
+                .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+            
+            Text("Seja bem-vindo ao Paladares")
+                .font(.custom("SF Pro", size: 32))
+                .multilineTextAlignment(.center)
+                .frame(width: 450, height: 80, alignment: .center)
+               
+            Spacer()
+            
+            // Botão de Início
+            Button(action: {
+                // Ação para ir para a próxima tela
+            }) {
+                Text("Iniciar")
+                    .font(.title2)
+                    .frame(width: 450, height: 80)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                    .padding(.horizontal, 40)
+            }
+            .padding(.bottom, 50)
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
