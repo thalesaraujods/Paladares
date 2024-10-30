4d1c197c26a430b6c921b8cd6745a19d357bd00b
amandrbl <amandarabelo.558@gmail.com>
2024-10-30 16:45:15 -0400

    fazendo a view de curiosidades

---
+++ Paladares/RecipeList/RecipeListCuriosidadeView.swift
@@ -0,0 +1,40 @@
+//
+//  RecipeListCuriosidadeView.swift
+//  Paladares
+//
+//  Created by Amanda Rabelo on 30/10/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct RecipeListCuriosidadeView: View {
+
+    @State var recipe: Recipe
+
+    var body: some View {
+        VStack {
+
+            Text("Curiosidade")
+                .font(.system(size: 36))
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+                .padding(.horizontal, -400)
+            
+            ZStack{
+                Rectangle()
+                    .frame(width: 850, height: 250)
+                    .cornerRadius(35)
+                    .shadow(radius: 10)
+                    .foregroundStyle(.white)
+                
+                Text(recipe.curiosidade)
+                    .padding(200)
+            }
+        }
+    }
+}
+
+#Preview {
+    RecipeListCuriosidadeView(recipe: recipes[0])
+}
