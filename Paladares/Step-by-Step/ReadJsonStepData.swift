17d92a049470b0b120e98166ce21d79b68583a81
amandrbl <amandarabelo.558@gmail.com>
2024-10-31 22:06:16 -0400

    banco de dados dos passo-a-passo

---
+++ Paladares/Step-by-Step/ReadJsonStepData.swift
@@ -0,0 +1,8 @@
+//
+//  ReadJsonStepData.swift
+//  Paladares
+//
+//  Created by Amanda Rabelo on 31/10/24.
+//
+
+import Foundation
---
+++ Paladares/Step-by-Step/Step-by-StepView.swift
@@ -0,0 +1,8 @@
+//
+//  Step-by-StepView.swift
+//  Paladares
+//
+//  Created by Amanda Rabelo on 31/10/24.
+//
+
+import Foundation
