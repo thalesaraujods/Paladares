d0ab2129b25e978aa24e3825f6a6378e3f1fb6ba
amandrbl <amandarabelo.558@gmail.com>
2024-10-31 22:19:47 -0400

    falta so aparecer os dados corretos conforme o json

--- Paladares/Step-by-Step/ReadJsonStepData.swift
+++ Paladares/Step-by-Step/ReadJsonStepData.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import Combine
+
+struct Step: Codable, Identifiable {
+    var id = UUID()
+    var name: String
+    var passos: String
+    var descricao: String
+}
+
+class ReadJsonStepData: ObservableObject {
+    @Published var steps: [Step] = []
+    
+    init() {
+        loadStepData()
+    }
+    
+    func loadStepData() {
+        guard let url = Bundle.main.url(forResource: "steps", withExtension: "json") else {
+            print("Failed to find steps.json")
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decodedData = try JSONDecoder().decode([Step].self, from: data)
+            self.steps = decodedData
+        } catch {
+            print("Failed to load data: \(error)")
+        }
+    }
+}
--- Paladares/Step-by-Step/Step-by-StepView.swift
+++ Paladares/Step-by-Step/Step-by-StepView.swift
@@ -6,3 +6,84 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct StepbyStepView: View {
+
+    @ObservedObject var step = ReadJsonStepData()
+    
+    var body: some View {
+        TabView {
+            ForEach(step.steps){ step in
+                // Passo 1
+                VStack {
+                    Text(step.passos)
+                        .font(.custom("SF Pro", size: 48))
+                        .foregroundStyle(.corCinza2)
+                        .padding(.leading, -310)
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 640, height: 450)
+                            .cornerRadius(35)
+                            .foregroundStyle(.white)
+                        
+                        Text(step.descricao)
+                            .font(.custom("SF Pro", size: 28))
+                            .frame(width: 600, height: 200)
+                            .padding(.bottom, 300)
+                    }
+                }
+                .padding(.leading, 350)
+                
+                // Passo 2
+                VStack {
+                    Text(step.passos)
+                        .font(.custom("SF Pro", size: 48))
+                        .foregroundStyle(.corCinza2)
+                        .padding(.leading, -310)
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 640, height: 450)
+                            .cornerRadius(35)
+                            .foregroundStyle(.white)
+                        
+                        Text(step.descricao)
+                            .font(.custom("SF Pro", size: 28))
+                            .frame(width: 600, height: 200)
+                            .padding(.bottom, 300)
+                    }
+                }
+                .padding(.leading, 350)
+                
+                // Passo 3
+                VStack {
+                    Text(step.passos)
+                        .font(.custom("SF Pro", size: 48))
+                        .foregroundStyle(.corCinza2)
+                        .padding(.leading, -310)
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 640, height: 450)
+                            .cornerRadius(35)
+                            .foregroundStyle(.white)
+                        
+                        Text(step.descricao)
+                            .font(.custom("SF Pro", size: 28))
+                            .frame(width: 600, height: 200)
+                            .padding(.bottom, 300)
+                    }
+                }
+                .padding(.leading, 350)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always)) // Para exibir os pontos
+    }
+}
+
+#Preview {
+    StepbyStepView()
+}
