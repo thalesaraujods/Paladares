71b9634a2aa9c223b0e7c1bf0eae4cf4ff4ec661
Thales Araújo <t.araujods@gmail.com>
2024-11-14 14:18:51 -0400

    botão de voltar da RecipePreparationView implementado

--- Paladares/Recipe/Preparation/Template/ButtonBackView.swift
+++ Paladares/Recipe/Preparation/Template/ButtonBackView.swift
@@ -8,23 +8,47 @@
 import SwiftUI
 
 struct ButtonBackView: View {
+    
+    @EnvironmentObject private var coordinator: Coordinator
+    @Binding var showExitConfirmation: Bool
+    
     var body: some View {
-        Image(systemName: "xmark")
-            .resizable()
-            .frame(width: 60, height: 60)
-            .font(.system(.body))
-            .fontWeight(.semibold)
-            .foregroundColor(Color(red: 0.24, green: 0.24, blue: 0.24).opacity(0.5))
-            .cornerRadius(1000)
-            .background{
-                Circle()
-                    .frame(width: 100, height: 100)
-                    .foregroundStyle(Color(red: 0.5, green: 0.5, blue: 0.5).opacity(0.2))
+        HStack {
+            Button(action: {
+                showExitConfirmation = true
+            }) {
+                Image(systemName: "xmark")
+                    .resizable()
+                    .frame(width: 15, height: 15)
+                    .font(.system(.body))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.24, green: 0.24, blue: 0.24).opacity(0.5))
+                    .cornerRadius(1000)
+                    .background{
+                        Circle()
+                            .frame(width: 35, height: 35)
+                            .foregroundStyle(Color(red: 0.5, green: 0.5, blue: 0.5).opacity(0.2))
+                    }
+                    .padding()
+                
             }
+            Spacer()
+        }
+        .frame(width: 1300)
+        .alert("Você deseja mesmo sair da receita?", isPresented: $showExitConfirmation) {
+            Button("Não", role: .cancel) {
+                showExitConfirmation = false
+            }
+            Button("Sim", role: .destructive) {
+                coordinator.pop() // Retorna para a tela anterior
+            }
+        }
     }
 }
 
 
 #Preview {
-    ButtonBackView()
+    @Previewable @State var showExitConfirmation: Bool = true
+    
+    ButtonBackView(showExitConfirmation: $showExitConfirmation)
 }
